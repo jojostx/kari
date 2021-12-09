72ce99bc7938ddd8b70a314497f3d5844da43833
unknown <jojostx>
2021-12-09 18:26:37 +0100

    chore(UI):  UI update
    BREAKING CHANGES: none

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -101,7 +101,7 @@
 					<div class="px-2 py-6">
 						<h4 class="text-2xl font-bold cursor-pointer">Bronze <span class="text-base text-gray-500">(7%)</span></h4>
 						<div class="mt-6 mb-4">
-							<span class="text-xl font-extrabold text-blue-600">$25,000.00</span> Principal
+							<span class="text-xl font-extrabold text-blue-600">$25,000</span> Principal
 						</div>
 						<div class="flex mt-2">
 							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -132,7 +132,7 @@
 					<div class="px-2 py-6">
 						<h4 class="text-2xl font-bold cursor-pointer">Silver <span class="text-base text-gray-500">(7.5%)</span></h4>
 						<div class="mt-6 mb-4">
-							<span class="text-xl font-extrabold text-blue-600">$50,000.00</span> Principal
+							<span class="text-xl font-extrabold text-blue-600">$50,000</span> Principal
 						</div>
 						<div class="flex mt-2">
 							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -163,7 +163,7 @@
 					<div class="px-2 py-6">
 						<h4 class="text-2xl font-bold cursor-pointer">Gold <span class="text-base text-gray-500">(8%)</span></h4>
 						<div class="mt-6 mb-4">
-							<span class="text-xl font-extrabold text-blue-600">$150,000.00</span> Principal
+							<span class="text-xl font-extrabold text-blue-600">$150,000</span> Principal
 						</div>
 						<div class="flex mt-2">
 							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -195,7 +195,7 @@
 					<div class="px-2 py-6">
 						<h4 class="text-2xl font-bold cursor-pointer">Platinum <span class="text-base text-gray-500">(8.5%)</span></h4>
 						<div class="mt-6 mb-4">
-							<span class="text-xl font-extrabold text-blue-600">$200,000.00</span> Principal
+							<span class="text-xl font-extrabold text-blue-600">$200,000</span> Principal
 						</div>
 						<div class="flex mt-2">
 							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
