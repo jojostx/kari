c0c4481b8024e9a7b614ee56ee95a84658ef1e9f
unknown <jojostx>
2021-12-21 23:27:52 +0100

    chore (feat): add the contact-us functionality using livewire component
    add new custom validation rule classes
    
    add migrations for the contact_message table
    
    BREAKING CHANGES: contact-form uses livewire comp

--- app/Http/Livewire/Static/Forms/Contact.php
+++ app/Http/Livewire/Static/Forms/Contact.php
@@ -2,10 +2,43 @@
 
 namespace App\Http\Livewire\Static\Forms;
 
+use App\Models\ContactMessage;
+use App\Rules\StartsWith;
 use Livewire\Component;
 
 class Contact extends Component
 {
+    public $fullname;
+    public $phone;
+    public $email;
+    public $message;
+
+    protected function rules()
+    {
+        return [
+            'fullname' => ['required', 'max:255'],
+            'phone' => ['required_without:email', 'size:11', new StartsWith()],
+            'email' => ['required_without:phone', 'email'],
+            'message' => ['required', 'string', 'max:1024'],
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {        
+        $validated = $this->validate();
+
+        $contactMessage = ContactMessage::create($validated);
+
+        //this is where we send an email [queued job] to support containing the saved contactMessage
+        
+        \session()->flash('message', 'Your message has been recieved. A customer support agent will get in touch with you soon.');
+    }
+
     public function render()
     {
         return view('livewire.static.forms.contact');
