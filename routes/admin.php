00ce96713f29a72117775c71dce06858d5de66dc
unknown <jojostx>
2022-01-04 08:02:51 +0100

    chore(feat): redirect to dashboard from admin route

--- routes/admin.php
+++ routes/admin.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/', function () {
+  return redirect()->route('admin.dashboard');
+})->name('admin.dashboard');
+
 Route::get('/dashboard', function () {
   return view('pages.admin.dashboard');
 })->name('admin.dashboard');
