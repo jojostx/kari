d961ea26ee5c1fd4c99cbaa7cc8703a27dba4a16
unknown <jojostx>
2022-01-15 20:43:46 +0100

    chore (fix, CI/CD): run npm install after composer install

--- resources/views/livewire/admin/app/navigation.blade.php
+++ resources/views/livewire/admin/app/navigation.blade.php
@@ -143,7 +143,7 @@
                             </div>
                         </li>
                         <li>
-                            <a href="https://demo.filamentadmin.com/shop/brands" class="flex items-center gap-3 px-3 py-2 font-medium transition rounded-lg hover:bg-gray-500/5 focus:bg-gray-500/5">
+                            <a href="#" class="flex items-center gap-3 px-3 py-2 font-medium transition rounded-lg hover:bg-gray-500/5 focus:bg-gray-500/5">
                                 <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
                                 </svg>
