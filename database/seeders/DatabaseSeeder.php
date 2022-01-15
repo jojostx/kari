d83c4243a0a8cd72b50d764fadac9c8fe68ceb3f
unknown <jojostx>
2022-01-15 21:20:51 +0100

    chore (CI/CD): add console command to seed faker data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Newsfeed;
-use App\Models\NewsfeedSubscription;
-use App\Models\Payment;
-use App\Models\Subscription;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Models\Newsfeed;
+use App\Models\NewsfeedSubscription;
+use App\Models\Payment;
+use App\Models\Subscription;
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +22,40 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('setup:prod', function () {
+    $this->info("setting up database for feature preview");
+
+    $users = User::factory()
+        ->verified()
+        ->verified_phone()
+        ->count(50)
+        ->create();
+
+
+    $users->each(function (User $user) {
+        $payment = Payment::factory()->create(['user_id' => $user->id]);
+
+        $subscription = Subscription::factory()->create(['user_id' => $user->id, 'payment_id' => $payment->id, 'refcode' => $payment->refcode, 'ends_at' => now()->addYear()]);
+
+        $subscription->customer()->associate($user);
+
+        $payment->customer()->associate($user);
+    });
+
+    $users->each(function (User $user) {
+        $payment = Payment::factory()->create(['user_id' => $user->id, 'status' => false, 'refcode' => null]);
+
+        $payment->customer()->associate($user);
+    });
+
+    Newsfeed::factory()
+        ->count(20)
+        ->create();
+
+    NewsfeedSubscription::factory()
+        ->count(20)
+        ->create();
+
+    $this->info('The command was successful!');
+})->purpose('sets up the database for feature preview by seeding fake data');
