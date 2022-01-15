c3796abdf365dc00190346b5f3c33bbe29b5f063
unknown <jojostx>
2022-01-15 20:36:15 +0100

    chore (fix, CI/CD): run npm install after composer install

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,37 +19,5 @@ class DatabaseSeeder extends Seeder
             FaqSeeder::class,
             PlanSeeder::class,
         ]);
-
-        $users =  User::factory()
-            ->verified()
-            ->verified_phone()
-            ->count(50)
-            ->create();
-
-
-        $users->each(function (User $user) {
-            $payment = Payment::factory()->create(['user_id' => $user->id]);
-
-            $subscription = Subscription::factory()->create(['user_id' => $user->id, 'payment_id' => $payment->id, 'refcode' => $payment->refcode, 'ends_at' => now()->addYear()]);
-
-            $subscription->customer()->associate($user);
-
-            $payment->customer()->associate($user);
-        });
-
-        $users->each(function (User $user) {
-            $payment = Payment::factory()->create(['user_id' => $user->id, 'status' => false, 'refcode' => null]);
-
-            $payment->customer()->associate($user);
-        });
-
-        Newsfeed::factory()
-            ->count(20)
-            ->create();
-
-
-        NewsfeedSubscription::factory()
-            ->count(20)
-            ->create();
     }
 }
