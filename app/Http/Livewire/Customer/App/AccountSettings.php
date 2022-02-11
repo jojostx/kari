d3f608baa7dfcfb27e6c839439dbcb6e5f65dba7
Ari Jay <ikuskid7@gmail.com>
2022-02-11 22:25:50 +0100

    chore(WIP/FIX): implement password reset functionality
    fix CI/CD failure by restoring 'alert-toast-cookie' blade component to the views\resources\components folder

--- app/Http/Livewire/Customer/App/AccountSettings.php
+++ app/Http/Livewire/Customer/App/AccountSettings.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire\Customer\App;
 
-use App\Models\City;
 use App\Models\PendingUserPhoneNumber;
 use App\Models\State;
 use App\Models\User;
@@ -13,6 +12,9 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class AccountSettings extends Component implements HasForms
@@ -147,29 +149,30 @@ class AccountSettings extends Component implements HasForms
                 'default' => 1,
                 'md' => 2,
             ])->schema([
-                    TextInput::make('current_password')
-                        ->label('Current Password')
-                        ->password()
-                        ->rules(['required_with:new_password'])
-                        ->currentPassword()
-                        ->autocomplete('off')
-                        ->columnSpan(1),
-                    Grid::make()
-                        ->schema([
-                            TextInput::make('new_password')
-                                ->label('New Password')
-                                ->password()
-                                ->rules(['confirmed'])
-                                ->autocomplete('new-password'),
-                            TextInput::make('new_password_confirmation')
-                                ->label('Confirm Password')
-                                ->password()
-                                ->rules([
-                                    'required_with:new_password',
-                                ])
-                                ->autocomplete('new-password'),
-                        ]),
-                ]),
+                TextInput::make('current_password')
+                    ->label('Current Password')
+                    ->password()
+                    ->rules(['required_with:new_password'])
+                    ->currentPassword()
+                    ->autocomplete('off')
+                    ->columnSpan(1),
+                Grid::make()
+                    ->schema([
+                        TextInput::make('new_password')
+                            ->label('New Password')
+                            ->password()
+                            ->rules(['confirmed'])
+                            ->autocomplete('new-password'),
+                        TextInput::make('new_password_confirmation')
+                            ->label('Confirm Password')
+                            ->password()
+                            ->rules([
+                                'required_with:new_password',
+                            ])
+                            ->dehydrated(false)
+                            ->autocomplete('new-password'),
+                    ]),
+            ]),
         ];
     }
 
@@ -221,9 +224,17 @@ class AccountSettings extends Component implements HasForms
 
     public function savePasswordInfo(): void
     {
-        \dd($this->passwordInfoForm->getState());
-    }
+        $status =  $this->authUser->forceFill([
+            'password' => Hash::make($this->passwordInfoForm->getState()['new_password']),
+            'remember_token' => Str::random(60),
+        ])->save();
 
+        if ($status) {
+            event(new PasswordReset($this->authUser));
+
+            $this->savedPasswordInfo = true;
+        }
+    }
 
     public function render()
     {
