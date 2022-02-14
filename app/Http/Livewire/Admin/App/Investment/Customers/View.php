0445f0ba82dddb734b7005b30e57f6c9b0aac395
Ari Jay <ikuskid7@gmail.com>
2022-02-14 11:02:26 +0100

    chore(fix): fix the contact form

--- app/Http/Livewire/Admin/App/Investment/Customers/View.php
+++ app/Http/Livewire/Admin/App/Investment/Customers/View.php
@@ -3,24 +3,60 @@
 namespace App\Http\Livewire\Admin\App\Investment\Customers;
 
 use App\Models\User;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Filament\Forms\Contracts\HasForms;
 use Livewire\Component;
 
-class View extends Component
+class View extends Component implements HasForms
 {
+    use InteractsWithForms;
+
     public User $customer;
+
+    public $first_name;
+    public $middle_name;
+    public $last_name;
+    public $email;
+    public $phone_number;
     
-    protected $rules = [
-        'customer.location.address' => ['required', 'string'],
-        'customer.phone_number' => ['required', 'string'], 
-        'customer.email' => ['required', 'email'], 
-        'customer.first_name' => ['required', 'string', 'max:124'], 
-        'customer.middle_name' => ['required', 'string', 'max:124'], 
-        'customer.last_name' => ['required', 'string', 'max:124'], 
-    ];
+    public $updated = false;
+
+    public function mount(): void
+    {
+        $this->form->fill([
+            'first_name' => $this->customer->first_name,
+            'middle_name' => $this->customer->middle_name,
+            'last_name' => $this->customer->last_name,
+            'email' => $this->customer->email,
+            'phone_number' => $this->customer->phone_number,
+        ]);
+    }
+
+    protected function getFormModel(): User
+    {
+        return $this->customer;
+    }
+
+    protected function getFormSchema(): array
+    {
+
+        return [
+            Grid::make()
+                ->schema([
+                    TextInput::make('first_name')->required(),
+                    TextInput::make('middle_name')->required(),
+                    TextInput::make('last_name')->required(),
+                    TextInput::make('email')->required(),
+                    TextInput::make('phone_number')->required(),
+                ])
+        ];
+    }
 
     public function save()
     {
-        return redirect()->route('admin.investment.customers.index');
+        $this->updated =$this->customer->update($this->form->getState());
     }
 
     public function render()
